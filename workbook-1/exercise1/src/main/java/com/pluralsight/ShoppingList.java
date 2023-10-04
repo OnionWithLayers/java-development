c0bfc92b5fc93b6f45baeb4104ec500ee3c80ae3
DKim-appdev <danhkim2003@gmail.com>
2023-10-04 09:44:35 -0400

    New Java Exercise1

---
+++ workbook-1/exercise1/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("milk");
+        System.out.println("bread");
+        System.out.println("eggs");
+        System.out.println("meat");
+        System.out.println("candy");
+        System.out.println("gum");
+        System.out.println("chocolate");
+        System.out.println("chips");
+        System.out.println("juice");
+        System.out.println("fruits");
+    }
+    }
+
