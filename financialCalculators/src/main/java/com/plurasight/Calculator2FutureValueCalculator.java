86c53cdcce5fc525874832b6c7fba355da9d9fbe
DKim-appdev <danhkim2003@gmail.com>
2023-10-11 08:34:41 -0400

    formatted the calculators

--- financialCalculators/src/main/java/com/plurasight/Calculator2FutureValueCalculator.java
+++ financialCalculators/src/main/java/com/plurasight/Calculator2FutureValueCalculator.java
@@ -1,25 +1,31 @@
 package com.plurasight;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Calculator2FutureValueCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //deposit is $1000
         System.out.println("Enter your deposit:");
         int deposit = scanner.nextInt();
 
+        //interest rate 1.75%
         System.out.println("Enter your interest rate:");
         double interestRate = scanner.nextDouble();
         double realInterestRate = interestRate / 100;
 
+        //5 years
         System.out.println("Enter your number of years:");
         int years = scanner.nextInt();
 
         double futureValue = deposit * Math.pow((1 + realInterestRate), years);
-        System.out.println("Your Future Value will be: " + futureValue);
+        String formattedFutureValue = NumberFormat.getCurrencyInstance().format(futureValue);
+        System.out.println("Your Future Value will be: " + formattedFutureValue);
 
         double totalInterestEarned = futureValue - deposit;
-        System.out.println("You have earned " + totalInterestEarned + "in interest");
+        String formattedTotalInterestEarned = NumberFormat.getCurrencyInstance().format(totalInterestEarned);
+        System.out.println("You have earned " + formattedTotalInterestEarned + " in interest");
     }
 }
